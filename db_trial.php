d5fa7f4b1c4b0cfd90d7cbef1345507d84aae82c
Jacob okoro <jaycobokoro@gmail.com>
2017-08-23 22:05:36 +0200

    For hng

---
+++ db_trial.php
@@ -0,0 +1,59 @@
+<?php
+	/**
+	SImple php script file.
+	*/
+
+
+	simple php file.
+	include_once ('database.php');
+	include_once ('students.php');
+
+
+	$database->connection->query($sql);
+	
+	$sql = "SELECT * FROM students";
+
+	$result = $database->connection->query($sql);
+
+	$results = $result->fetchAll(PDO::FETCH_CLASS,'students');
+
+	foreach ($results as $obj){
+		echo $obj->mat_no.' = > '.$obj->get_fullname().' = > '.$obj->status.'<br>';
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+/*
+	$mat_no='U2014/5575017';
+	$first_name = "Franklyn";
+	$last_name = "Thomas";
+	
+	$sql = "INSERT INTO students (mat_no,first_name,last_name) 
+				VALUES ('$mat_no','$first_name','$last_name')";
+
+	$database->connection->query($sql);
+*/
+/*
+	$sql = "UPDATE students SET status = '0' WHERE mat_no = 'U2014/5575017'";
+*/
+?>
